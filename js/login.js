5d4a4394db130def9b61df23e29a8944840e312b
mikacab2471 <88199791+mikacab2471@users.noreply.github.com>
2021-08-20 14:40:32 -0300

    obligatorio 1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,36 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+
+    document.getElementById("enviarInfo").addEventListener("click", function () {
+
+
+        let inputEmail = document.getElementById("iEmail");
+        let inputPassword = document.getElementById("iPassword");
+        let completado = true;
+
+        if (inputEmail.value === '') {
+            inputEmail.classList.add("invalid");
+            completado = false;
+        } else {
+            inputEmail.classList.remove("invalid");
+        }
+        if (inputPassword.value === '') {
+            inputPassword.classList.add("invalid");
+            completado = false;
+        } else {
+            inputPassword.classList.remove("invalid");
+        }
+        
+        if (completado) {
+            window.location = "inicioPag.html";
+        } else {
+            alert("Usuario o contraseña incorrecta")
+        }
+
+
+    })
+
 
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,45 @@
+var listaProductos = [];
+
+function showProductos(array) {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let producto = array[i];
+
+        htmlContentToAppend += `
+        <div>
+            <div class="row">
+                <div class="bloque-productos">
+                    <div>
+                      <img src="` + producto.imgSrc + `" alt="` + producto.description + `" class="img-estilo">
+                    </div>
+                    <div>
+                     <div>
+                          <h4 >`+ producto.name + `</h4>
+                          <p>`+ producto.description + `</p>
+                          <h5>`+ producto.currency + ` ` + producto.cost + `</h5>
+                     </div>
+                        
+                        <small class="text-muted">` + producto.soldCount + ` artículos vendidos</small>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+       `
+
+        document.getElementById("listado-p").innerHTML = htmlContentToAppend;
+    }
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (response) {
+        if (response.status === "ok") {
+            listaProductos = response.data;
 
+            showProductos(listaProductos);
+        }
+    })
 });
